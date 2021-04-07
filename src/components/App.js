339fd7fe9f2223cac2c1dcc1f0e0b6248f45bd0c
Yu Shi <yushiys95@gmail.com>
2021-04-06 21:47:29 -0400

    DQ

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,15 @@
 import React from "react";
+import TopMenu from "./TopMenu";
+import SideMenu from "./SideMenu"
+import MainContent from "./MainContent";
+
 
 function App() {
-  return <div className="app-container">{/* What components go here? */}</div>;
+  return <div className="app-container">
+  <TopMenu />
+  <SideMenu />
+  <MainContent />
+  </div>;
 }
 
 export default App;
